fec74acf9617f5f11e48629e246aef6d0e9c799c
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-12 13:50:25 +0900

    Create build.ts

---
+++ scripts/build.ts
@@ -0,0 +1,26 @@
+import { build, emptyDir } from "dnt"
+
+await emptyDir("./dist")
+
+await build({
+  entryPoints: ["./src/index.ts"],
+  outDir: "./dist",
+  shims: {},
+  package: {
+    name: "mikanui",
+    version: Deno.args[0],
+    description: "MikanUI🍊 is a library of Tailwind CSS like Material Design 3.",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/nakasyou/MikanUI.git",
+    },
+    bugs: {
+      url: "https://github.com/nakasyou/MikanUI/issues",
+    },
+  },
+  postBuild() {
+    Deno.copyFileSync("LICENSE", "dist/LICENSE");
+    Deno.copyFileSync("README.md", "dist/README.md");
+  },
+});
