e0127518c16377b8df798c36c85c736e3f7ed156
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-12 14:00:25 +0900

    Create material-color.ts

---
+++ src/material-color.ts
@@ -0,0 +1,30 @@
+import type { MikanUIInit } from './mod.ts'
+import { argbFromHex, themeFromSourceColor, applyTheme, hexFromArgb } from 'npm:@material/material-color-utilities'
+
+export default (init: MikanUIInit) => {
+  const theme = themeFromSourceColor(argbFromHex(init.seedColor))
+  const props = Object.keys(theme.schemes.light.props)
+  const classes = props.map(prop => prop.replace(/[A-Z]/g, l => '-' + l.toLowerCase()))
+  const classToCss = Object.entries({
+    bg: 'background-color',
+    text: 'color',
+    border: 'border-color',
+    decoration: 'text-decoration-color',
+    shadow: '--tw-shadow-color',
+  })
+  const lightComponents = Object.assign(...classToCss.map(([className, cssKey]) => Object.fromEntries(classes.map((query, index) => {
+    const cssObject = {}
+    cssObject[cssKey] = hexFromArgb(theme.schemes.light[props[index]])
+    return ['.' + className + '-' + query, cssObject]
+  }))))
+  const darkComponents = Object.assign(...classToCss.map(([className, cssKey]) => Object.fromEntries(classes.map((query, index) => {
+    const cssObject = {}
+    cssObject[cssKey] = hexFromArgb(theme.schemes.dark[props[index]])
+    return ['.' + className + '-' + query, cssObject]
+  }))))
+
+  return {
+    '@media (prefers-color-scheme: dark)': darkComponents,
+    '@media (prefers-color-scheme: light)': lightComponents,
+  }
+}
